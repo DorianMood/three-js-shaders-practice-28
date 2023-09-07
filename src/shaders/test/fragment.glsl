14e072e35d0d88aecc6a71552376ac5fdcb2b296
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-09-07 20:48:38 +0300

    exercise 18

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -7,7 +7,7 @@ void main()
     float intensityX = abs((vUv.x - 0.5) * strength);
     float intensityY = abs((vUv.y - 0.5) * strength);
 
-    float intensity = min(intensityX, intensityY);
+    float intensity = max(intensityX, intensityY);
 
     gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
 }
