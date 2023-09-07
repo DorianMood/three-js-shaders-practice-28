080613602ff28c161eed172e07b3809820bc5472
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-09-07 20:28:02 +0300

    exercise 12

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,7 +3,7 @@ varying vec2 vUv;
 void main()
 {
     float intensity = step(0.8, mod(vUv.x * 10.0, 1.0));
-    intensity += step(0.8, mod(vUv.y * 10.0, 1.0));
+    intensity *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
     gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
 }
