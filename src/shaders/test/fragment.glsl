49e5011c0de4c0364b95a0857e311130973b6df7
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-09-07 20:38:00 +0300

    exercise 15

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,10 +3,10 @@ varying vec2 vUv;
 void main()
 {
     float intensityX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    intensityX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    intensityX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
 
     float intensityY = step(0.4, mod(vUv.y * 10.0, 1.0));
-    intensityY *= step(0.8, mod(vUv.x * 10.0, 1.0));
+    intensityY *= step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
 
     float intensity = intensityX + intensityY;
 
