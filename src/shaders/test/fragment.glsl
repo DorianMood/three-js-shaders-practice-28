70c41a78fdc24d70a69fb251ee06af24591d5653
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-09-08 21:41:15 +0300

    exercise 21

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -2,16 +2,7 @@ varying vec2 vUv;
 
 void main()
 {
-    float strength = 2.0;
-
-    float intensityX = abs((vUv.x - 0.5) * strength);
-    float intensityY = abs((vUv.y - 0.5) * strength);
-
-    float square1 = step(0.2, max(intensityX, intensityY));
-    float square2 = 1.0 - step(0.25, max(intensityX, intensityY));
-
-
-    float intensity = square1 * square2;
+    float intensity = floor(vUv.x * 10.0) / 10.0;
 
     gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
 }
