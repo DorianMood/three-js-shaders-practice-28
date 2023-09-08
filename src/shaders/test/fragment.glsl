1bd9014fb86b4504d5f08500f6239396a25f8ebc
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-09-08 21:46:40 +0300

    exercise 23

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,11 +1,13 @@
 varying vec2 vUv;
 
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
 void main()
 {
-    float x = floor(vUv.x * 10.0) / 10.0;
-    float y = floor(vUv.y * 10.0) / 10.0;
-
-    float intensity = x * y;
+    float intensity = random(vUv);
 
     gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
 }
